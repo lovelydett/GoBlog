76a84e8da90363d286f4b0400b60b2443e806b04
Yuting.Xie <yuting.xie@weride.ai>
2021-04-27 14:49:48 +0800

    implement front-end and back-end categories data-flow

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,71 +2,87 @@ package controllers
 
 import (
 	"GinBlog/model"
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 //日志系统
 var logInf = log.New(os.Stdout, "[INFO]", log.LstdFlags)
-var logErr = log.New(os.Stdout, "[Error]", log.LstdFlags | log.Lshortfile)
+var logErr = log.New(os.Stdout, "[Error]", log.LstdFlags|log.Lshortfile)
 
 //cookie判断
-func isLogin(c *gin.Context)bool{
+func isLogin(c *gin.Context) bool {
 	_, e := c.Cookie("adminCookie")
-	return e==nil
+	return e == nil
 }
 
-//home页面的GET请求
-func HomeGet(c *gin.Context){
+// home页面的GET请求
+func HomeGet(c *gin.Context) {
 	logInf.Println("Entering HomeGet")
 
-	//附带上登录状态信息以便于前端可以根据相应的状态显示相应内容
-	c.HTML(http.StatusOK,"home.html", nil)
+	// handle 文章分类标签
+	categories := []model.Category{}
+	model.GetCategories(&categories)
+
+	c.HTML(http.StatusOK, "home.html", gin.H{
+		"Categories": categories,
+	})
 
 	logInf.Println("Leaving HomeGet")
 }
 
 //login页面的GET
-func LoginGet(c *gin.Context){
+func LoginGet(c *gin.Context) {
 	logInf.Println("Entering LoginGet")
 
-	c.HTML(http.StatusOK,"login.html",nil)
+	c.HTML(http.StatusOK, "login.html", nil)
 	//c.String(http.StatusOK,"ok")
 
 	logInf.Println("Leaving LoginGet")
 }
 
 //404页面的GET请求
-func NotFoundGet(c *gin.Context){
-	logInf.Println("Entering NotFoundGet for: "+c.Request.URL.String())
+func NotFoundGet(c *gin.Context) {
+	logInf.Println("Entering NotFoundGet for: " + c.Request.URL.String())
 
-	c.HTML(http.StatusNotFound,"404.html",nil)
+	c.HTML(http.StatusNotFound, "404.html", nil)
 
 	logInf.Println("Leaving NotFoundGet")
 }
 
 //article页面的GET请求
-func ArticleGet(c *gin.Context){
+func ArticleGet(c *gin.Context) {
 	logInf.Println("Entering ArticleGet")
 
-	//todo:根据页面号进行文章详情的查询
+	// handle文章列表
+	// todo:分页显示文章列表
 	articles := []model.Article{}
 	model.GetArticles(0, &articles)
+
+	// handle分类列表
+	categories := []model.Category{}
+	model.GetCategories(&categories)
+
+	// handle登录状态
 	login := isLogin(c)
-	c.HTML(http.StatusOK,"article.html",gin.H{
-		"IsLogin" : login,
-		"Articles":articles,
+
+	// 打包模板内容
+	c.HTML(http.StatusOK, "article.html", gin.H{
+		"IsLogin":    login,
+		"Categories": categories,
+		"Articles":   articles,
 	})
 
 	logInf.Println("Leaving ArticleGet")
 }
 
 //editArticle页面的GET请求
-func EditArticleGet(c *gin.Context){
+func EditArticleGet(c *gin.Context) {
 	logInf.Println("Entering EditArticleGet for article: ")
 	isNew := ("yes" == c.Request.FormValue("isNew"))
 	article := &model.Article{}
@@ -85,68 +101,69 @@ func EditArticleGet(c *gin.Context){
 
 	//模板传参
 	c.HTML(http.StatusOK, "editArticle.html", gin.H{
-		"IsNew" : isNew,
-		"Id" : article.ID,
-		"Title" : article.Title,
-		"Content" : article.Content,
+		"IsNew":   isNew,
+		"Id":      article.ID,
+		"Title":   article.Title,
+		"Content": article.Content,
 	})
 
 	logInf.Println("Leaving EditArticleGet for article: ")
 }
 
-type EditArtJson struct{
-	IsNew string `json:"isNew"`
-	IdStr string `json:"id"`
-	Title string `json:"title"`
+type EditArtJson struct {
+	IsNew   string `json:"isNew"`
+	IdStr   string `json:"id"`
+	Title   string `json:"title"`
 	Content string `json:"content"`
 }
+
 //editArticle页面的POST请求
-func EditArticlePost(c *gin.Context){
+func EditArticlePost(c *gin.Context) {
 	logInf.Println("Entering EditArticlePost: ")
 
 	//需要解析JSON
 	artJson := EditArtJson{}
-	if nil!=c.BindJSON(&artJson){
+	if nil != c.BindJSON(&artJson) {
 		logErr.Println("unable to unmarshal JSON posted from editArticle.html")
 		logInf.Println("Leaving EditArticlePost for article: ")
-		c.JSON(http.StatusOK,gin.H{"status":1, "message":"Unable to post an article, bind JSON error", "data":nil})
+		c.JSON(http.StatusOK, gin.H{"status": 1, "message": "Unable to post an article, bind JSON error", "data": nil})
 		return
 	}
 
-	logInf.Println("JSON from editArticle: "+artJson.IdStr, ",", artJson.IsNew, ",",artJson.Title)
+	logInf.Println("JSON from editArticle: "+artJson.IdStr, ",", artJson.IsNew, ",", artJson.Title)
 	if "yes" == artJson.IsNew {
 		logInf.Println("Inserting into db a new article")
 		//处理新文章入库的流程
-		model.AddArticleByInf(artJson.Title, artJson.Content,1)
+		model.AddArticleByInf(artJson.Title, artJson.Content, 1)
 		//todo:入库失败校验以及对应的状态码返回
-	}else{
+	} else {
 		logInf.Println("Updating an existing article in db:", artJson.Title)
 		//处理编辑旧文章的页面
 		id, _ := strconv.ParseInt(artJson.IdStr, 10, 64)
-		article := &model.Article{ID:id, Title: artJson.Title, Content: artJson.Content}
+		article := &model.Article{ID: id, Title: artJson.Title, Content: artJson.Content}
 		model.UpdateArticle(id, article)
 		if model.Error {
-			c.JSON(http.StatusOK,gin.H{"status":1, "message":"Unable to update article "+article.Title, "data":nil})
+			c.JSON(http.StatusOK, gin.H{"status": 1, "message": "Unable to update article " + article.Title, "data": nil})
 			return
 		}
 	}
 
 	//上传文章的ajax请求要的内容是json，所以这里也应该返回json而不是html
 	//要符合格式：
-	c.JSON(http.StatusOK,gin.H{"status":0, "message":"Posted an article", "data":nil})
-	logInf.Println("Leaving EditArticlePost for article: ",artJson.Title)
+	c.JSON(http.StatusOK, gin.H{"status": 0, "message": "Posted an article", "data": nil})
+	logInf.Println("Leaving EditArticlePost for article: ", artJson.Title)
 }
 
 //readArticle的GET请求（获取对应id的文章内容）
-func ReadArticleGet(c *gin.Context){
+func ReadArticleGet(c *gin.Context) {
 	logInf.Println("Entering ReadArticleGet: ")
 
 	//先获取文章id并转int64
-	idStr:=c.Request.FormValue("id")
-	id,_ := strconv.ParseInt(idStr,10,64)
+	idStr := c.Request.FormValue("id")
+	id, _ := strconv.ParseInt(idStr, 10, 64)
 
 	//通过ID查表
-	art := model.Article{ID:id}
+	art := model.Article{ID: id}
 	model.GetArticleById(id, &art)
 
 	//检验结果
@@ -154,10 +171,10 @@ func ReadArticleGet(c *gin.Context){
 		logErr.Println("cannot find article by id: ", id)
 	}
 
-	c.HTML(http.StatusOK,"readArticle.html",gin.H{
-		"IsLogin" : isLogin(c),
-		"Id": art.ID,
-		"Title": art.Title,
+	c.HTML(http.StatusOK, "readArticle.html", gin.H{
+		"IsLogin": isLogin(c),
+		"Id":      art.ID,
+		"Title":   art.Title,
 		"Content": art.Content,
 	})
 
@@ -165,42 +182,41 @@ func ReadArticleGet(c *gin.Context){
 }
 
 //deleteArticle的POST请求（删除指定id的文章）
-func DeleteArticlePost(c *gin.Context){
+func DeleteArticlePost(c *gin.Context) {
 	logInf.Println("Entering DeleteArticlePost: ")
 
 	//这里是从url解析参数而不是解析json，别搞错了
 	//先获取文章id并转为int64
-	idStr:=c.Request.FormValue("id")
-	id,_ := strconv.ParseInt(idStr,10,64)
+	idStr := c.Request.FormValue("id")
+	id, _ := strconv.ParseInt(idStr, 10, 64)
 
 	//交给model模块删除之
 	logInf.Println("Deleting article with id:", idStr)
 	model.DeleteArticle(id)
 
 	//检查model模块的全局变量Error，看看是否有异常发生
-	if model.Error{
+	if model.Error {
 		//返回json的统一格式：{code:200,obj:{status:0(成功)/-1（失败）,message:"content",data:"content"}}
-		c.JSON(http.StatusOK,gin.H{"status":1,"message":"Error deleting article","data":""})
-	}else{
-		c.JSON(http.StatusOK,gin.H{"status":0,"message":"Article deleted","data":""})
+		c.JSON(http.StatusOK, gin.H{"status": 1, "message": "Error deleting article", "data": ""})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"status": 0, "message": "Article deleted", "data": ""})
 	}
-
 
 	logInf.Println("Leaving DeleteArticlePost for article id = : ", id)
 }
 
 //login的POST请求（登录）
-func LoginPost(c *gin.Context){
+func LoginPost(c *gin.Context) {
 	logInf.Println("Entering LoginPost")
 
 	//从url请求中获取密码
-	pwStr:=c.Request.FormValue("pw")
-	if strings.Compare(pwStr, "981123")==0 {
+	pwStr := c.Request.FormValue("pw")
+	if strings.Compare(pwStr, "981123") == 0 {
 		//设置一条名叫adminCookie的cookie
 		c.SetCookie("adminCookie", "tt", 3600, "/", "localhost", false, true)
-		c.JSON(200, gin.H{"status":0,"message":"Login Successful","data":""})
-	}else{
-		c.JSON(200, gin.H{"status":-1,"message":"Wrong Password","data":""})
+		c.JSON(200, gin.H{"status": 0, "message": "Login Successful", "data": ""})
+	} else {
+		c.JSON(200, gin.H{"status": 1, "message": "Wrong Password", "data": ""})
 	}
 
 	logInf.Println("Leaving LoginPost")
--- model/model.go
+++ model/model.go
@@ -1,13 +1,13 @@
 package model
 
-import(
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+import (
 	"log"
 	"os"
-
-	// _ "github.com/jinzhu/gorm/dialects/mysql"
 	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	// _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 //错误标志位，类似gorm的方式来指示操作有无成功
@@ -15,33 +15,34 @@ var Error bool
 
 //日志
 var logInf = log.New(os.Stdout, "[INFO]", log.LstdFlags)
-var logErr = log.New(os.Stdout, "[Error]", log.LstdFlags | log.Lshortfile)
-
+var logErr = log.New(os.Stdout, "[Error]", log.LstdFlags|log.Lshortfile)
 
 type Category struct {
-	ID int64 `gorm:"primary_key"`
+	ID   int64  `gorm:"primary_key"`
 	Name string `json:"name"`
 	//NumofArticles int64 ?
 }
 
 //文章id要使用uuid!否则删除不方便，然后在id上建索引
-func getTimeStamp () int64 {
-	time.Sleep(2*time.Millisecond)//防止重复id，需要吗？看uuid原理
+func getTimeStamp() int64 {
+	time.Sleep(2 * time.Millisecond) //防止重复id，需要吗？看uuid原理
 	return time.Now().Unix()
 }
+
 type Article struct {
-	ID int64 `gorm:"index;primary_key;"`
-	Title string `json:"title"`
+	ID      int64  `gorm:"index;primary_key;"`
+	Title   string `json:"title"`
 	Content string `json:"content"`
 	// TimeInit int64 `orm:"index"` 不需要 uuid就是生成时间戳
-	TimeLastEdit int64 `gorm:"index;"`
-	ReaderCount int64 `gorm:"index;"`
-	Categories []Category `gorm:"index;many2many:article_category;"` //文章包含并属于多个分类
+	TimeLastEdit int64      `gorm:"index;"`
+	ReaderCount  int64      `gorm:"index;"`
+	Categories   []Category `gorm:"index;many2many:article_category;"` //文章包含并属于多个分类
 }
 
 //初始化model模块：数据库连接，校验等
 var db *gorm.DB
-func init(){
+
+func init() {
 	logInf.Println("Enter init of model.go")
 
 	//错误标志置为否
@@ -53,37 +54,37 @@ func init(){
 	db_, err := gorm.Open(dbType, dbName)
 
 	//校验数据库连接
-	if err!=nil {
+	if err != nil {
 		//logErr.Println("Enable to open DB")
-		panic("Enable to open DB: "+dbType+", "+dbName)
+		panic("Enable to open DB: " + dbType + ", " + dbName)
 	}
 
 	//赋值给全局变量
 	db = db_
 
 	//迁移
-	db.AutoMigrate(&Category{},&Article{})
+	db.AutoMigrate(&Category{}, &Article{})
 
 	//插入默认数据
-	cat := Category{ID:1, Name:"default"}
+	cat := Category{ID: 1, Name: "default"}
 	if db.NewRecord(cat) {
 		//还没有分类
 		logInf.Println("Auto create default category")
 		db.Create(&cat)
 	}
 
-	article := Article{ID:1, Title: "Default title", Content: "Default content"}
+	article := Article{ID: 1, Title: "Default title", Content: "Default content"}
 	if db.NewRecord(article) {
 		//还没有文章
 		logInf.Println("Auto create default article")
-		AddArticle(&article)
+		addArticle(&article)
 	}
 
 	logInf.Println("Leave init of model.go")
 }
 
-func closeDB(){
-	if nil!=db.Close(){
+func closeDB() {
+	if nil != db.Close() {
 		logErr.Println("Enable to close db")
 	}
 }
@@ -92,37 +93,48 @@ func closeDB(){
 
 //查询
 //通过ID查文章
-func GetArticleById(id int64, article *Article){
+func GetArticleById(id int64, article *Article) {
 	db.First(article, id)
 }
 
 //通过精确名称查找文章
-func GetArticleByTitle(title string, article *Article){
+func GetArticleByTitle(title string, article *Article) {
 	db.Where("title = ?", title).First(article)
 }
 
-func GetArticles(num int, articles *[]Article){
+func GetArticles(num int, articles *[]Article) {
 	logInf.Println("Enter GetArticle")
 	//if num<=0, get all
-	db.Find(articles)
 	//select * from article
+	db.Find(articles)
 
 	logInf.Println("Leave GetArticle")
 }
 
-
-func AddArticleByInf(title string, content string, catId int64){
-	logInf.Println("Enter AddArticleByInf: "+title)
-	//cats := []Category{{ID:catId}}
-	art := Article{ID:getTimeStamp(),Title: title, Content: content}
-	AddArticle(&art)
-	logInf.Println("Leave AddArticleByInf: "+title)
+// 查找指定分类
+func GetCategoryByName(name string, category *Category) {
+	logInf.Println("Enter GetCategoryByName")
+	db.Where("name = ?", name).First(category)
+	logInf.Println("Leave GetCategoryByName")
 }
 
-func AddArticle(article *Article) {
-	logInf.Println("Enter AddArticle")
+// 查找所有分类
+func GetCategories(categories *[]Category) {
+	logInf.Println("Enter GetCategories")
+	db.Find(categories)
+	logInf.Println("Leave GetCategories")
+}
+
+func AddArticleByInf(title string, content string, catId int64) {
+	logInf.Println("Enter AddArticleByInf: " + title)
+	//cats := []Category{{ID:catId}}
+	art := Article{ID: getTimeStamp(), Title: title, Content: content}
+	addArticle(&art)
+	logInf.Println("Leave AddArticleByInf: " + title)
+}
+
+func addArticle(article *Article) {
 	db.Create(article)
-	logInf.Println("Leave AddArticle")
 }
 
 //删除
@@ -130,10 +142,10 @@ func AddArticle(article *Article) {
 func DeleteArticle(id int64) {
 	logInf.Println("Enter DeleteArticle")
 
-	article := &Article{ID:id}
+	article := &Article{ID: id}
 	//检查是否还有这个id的文章
 	//todo:注意：这是gorm检查错误情况的标准写法，需要都改成这样
-	if err:=db.Delete(article).Error; err!=nil {
+	if err := db.Delete(article).Error; err != nil {
 		logInf.Println("Enable to delete article id = ", id)
 		Error = true
 		return
@@ -147,7 +159,7 @@ func DeleteArticle(id int64) {
 //通过id更新文章
 func UpdateArticle(id int64, article *Article) {
 	logInf.Println("Enter UpdateArticle for:", article.Title)
-	if err:=db.Save(article).Error; err!=nil { // Save update value in database, if the value doesn't have primary key, will insert it
+	if err := db.Save(article).Error; err != nil { // Save update value in database, if the value doesn't have primary key, will insert it
 		logInf.Println("Enable to update article:", article.Title)
 		Error = true
 		return
@@ -155,7 +167,3 @@ func UpdateArticle(id int64, article *Article) {
 	Error = false
 	logInf.Println("Leave UpdateArticle")
 }
-
-
-
-
